318b8c08fb1af2e18d11f7e3c56ebccd03fa877b
Pantelis Kourtidis <panteleskourtides@Panteliss-Air.router.wind>
2024-12-08 00:28:42 +0200

    Added Procfile and fixed start command

--- advizeapp_backend/database.py
+++ advizeapp_backend/database.py
@@ -1,15 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-# Database connection URL
-SQLALCHEMY_DATABASE_URL = "sqlite:///./advizeapp.db"
+# Φόρτωση .env
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# Παίρνουμε το URL της βάσης από το .env
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Ρύθμιση σύνδεσης
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Dependency for getting DB session
+# Dependency για σύνδεση με τη βάση
 def get_db():
     db = SessionLocal()
     try:
