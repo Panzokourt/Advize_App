812c3252037a26d95b1faf6e42732c1c40956ca8
Pantelis Kourtidis <panteleskourtides@Panteliss-MacBook-Air.local>
2024-12-10 16:45:43 +0200

    Update Service Page

--- advizeapp-frontend/src/index.js
+++ advizeapp-frontend/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap CSS import
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.render(
   <React.StrictMode>
